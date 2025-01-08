3b298077d0cf32fd3cb23b3f971cc83dbfe100e8
Abdullah Elsman <abdullahelsman294@gmail.com>
2025-01-08 08:17:04 +0200

    Boolean

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,11 @@
 // this is main function
 fun main() {
-    val message = """
-        Hello,
-        My Name is Mohammed.
-        How are you?
-    """.trimIndent()
-val message2 = """
-        Hello,
-        My Name is Ahmed.
-        How are you?
-    """.replaceIndent("- ")
+    var isBlank:Boolean = false
+    println(isBlank)
 
-    println(message)
-    println("==========================")
-    println(message2)
+    println(isBlank.not())
 
+    isBlank = "".isBlank()
 
-    val name = "Tamer"
-    val age = 25
-
-    println("Hello $name Your age is $age")
-    println("Hello $name your age is $age and your name is has ${name.length} Characters long")
-
-    println(name.length)
+    println(isBlank)
 }
