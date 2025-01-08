4359114173d449532a42f9c221818be803bac7ed
Abdullah Elsman <abdullahelsman294@gmail.com>
2025-01-08 09:53:18 +0200

    Range

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,16 @@
 // this is main function
 fun main()
 {
-    val values = listOf(1,2,3,4,5,6)
-    for (i in values){
-        print("$i ")
+    val i = 12;
+
+    if (i in 1..5){
+        println("Yes")
+    }
+    else {
+        println("No")
     }
 
-    println()
-
-    for(i in 0 until 10 ){
-        print("$i ")
-    }
-    println()
-
-    val start = 0
-    val stop = 78
-    for (i in start until stop step 3){
-        println(i)
-    }
-
-    for (i in 100 downTo 0 step 2){
+    for(i in 0..10 step 2){
         println(i)
     }
 }
