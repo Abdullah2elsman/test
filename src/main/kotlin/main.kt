7667542b6adebdf71170ce4e3bb5d68b70405a3d
Abdullah Elsman <abdullahelsman294@gmail.com>
2025-01-08 09:45:41 +0200

    For Loop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,25 @@
 // this is main function
-fun main() {
-    val number1 = 12.5
-    val number2 = 3.5
-    val result:Double
+fun main()
+{
+    val values = listOf(1,2,3,4,5,6)
+    for (i in values){
+        print("$i ")
+    }
 
-    result = number1 + number2
+    println()
 
-    println("Addition: $result")
+    for(i in 0 until 10 ){
+        print("$i ")
+    }
+    println()
 
-    val firstName = "Mohamed"
-    val lastName = "Tamer"
-    val job = "Developer"
+    val start = 0
+    val stop = 78
+    for (i in start until stop step 3){
+        println(i)
+    }
 
-    val stringResult = "$firstName $lastName $job"
-
-    println(stringResult)
+    for (i in 100 downTo 0 step 2){
+        println(i)
+    }
 }
