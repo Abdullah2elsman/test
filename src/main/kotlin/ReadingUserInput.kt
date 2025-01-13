a67ed1720ab34fe1ae8d8ca3e8aeebb7920f97f7
Abdullah Elsman <abdullahelsman294@gmail.com>
2025-01-13 10:14:13 +0200

    Reading User Input

--- src/main/kotlin/ReadingUserInput.kt
+++ src/main/kotlin/ReadingUserInput.kt
@@ -9,4 +9,13 @@ fun main(){
 //	println("Your Age is $age")
 //
 //	println(age.javaClass.name)
+
+	print("Enter Your Name: ")
+	val name = readln()!!.toString()
+	val value = name.split(' ')
+	val firstChar = value[0].substring(0,1)
+	val lastChar = value[1].substring(0,1)
+
+	println(firstChar+lastChar)
+
 }
